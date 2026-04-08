a585fcc83ebba29949aa01062d2e54391be4a7e1
ElBatAndres <andresjulianpiraga@gmail.com>
2026-04-08 17:00:54 -0500

    test: CI test

--- backend/src/purchases/purchase.repository.interface.ts
+++ backend/src/purchases/purchase.repository.interface.ts
@@ -1,7 +1,2 @@
 import { PurchaseRow, CreatePurchaseDTO, PurchaseDetailRow, PurchaseWithQR } from "./purchase.types";
 
-export interface IPurchaseRepository {
-  create(usuarioId: number, dto: CreatePurchaseDTO, total: number): Promise<PurchaseRow>;
-  findByUser(usuarioId: number): Promise<PurchaseDetailRow[]>;
-  findById(id: number): Promise<Omit<PurchaseWithQR, "qr_code"> | null>;
-}
